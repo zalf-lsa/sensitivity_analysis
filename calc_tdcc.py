Corrected calculation of Savage Scores and Top-Down-Concordance-Coefficient

calc_tdcc.py
@@ -6,7 +6,8 @@ import csv
 import re
 import numpy
 import sys
-
+import pandas
+import scipy.stats as ss
 
 ranking_file = "D:/daten_specka/ZALF/publications/2015-Time-dependent-SA/data/morris_ranking_primyield.csv"
 
@@ -19,16 +20,14 @@ Main method
 """
 def main():
 
-    #parameter_ids, parameter_names, rank_array = get_ranking_map(ranking_file)
-    #print(rank_array)
+    parameter_ids, parameter_names, rank_array = get_ranking_map(ranking_file)
+    print(rank_array)
     
-    #array = rank_array[:,[0,1]]
-
-    array=numpy.array([1,1,1,2,4,3,3,2,4,4,3,2]).reshape(4,3)
-    print("Array:", array)
-
-    savage_scores = get_savage_score_array (array)
-    print("Savage scores:\t", savage_scores)
+    # test array
+    #rank_array=numpy.array([1,3,2,2,1,3,3,2,1,4,4,4]).reshape(4,3)
+    
+    savage_scores = get_savage_score_array (rank_array)
+    
     calc_TDCC(savage_scores)
 
 ######################################################
@@ -52,9 +51,9 @@ def get_savage_score_array(r_array):
 
     for rank_index in range(different_ranking_count):                
         savage_score_array[:,rank_index] = calc_savage_scores(r_array[:,rank_index])        
-         
-    print (savage_score_array)
-
+    
+    print("\nSavage scores:\n", savage_score_array,"\n")
+    
     return savage_score_array
 
 ######################################################
@@ -72,12 +71,10 @@ def calc_savage_scores(ranks):
     for rank in ranks:
         savage = 0
         
-        start = n-rank+1
-        x=list(range(start, n+1))
-        
-        for j in x:
+        for j in range(rank, n+1):
             savage += 1/j
-        savage_scores.append(1-savage)
+        
+        savage_scores.append(savage) 
     
     return savage_scores
 
@@ -86,35 +83,35 @@ def calc_savage_scores(ranks):
 ######################################################
 
 def calc_TDCC(savage_score_array):
-    print("Calc TDCC")
+    print("Calculation of TDCC")
+
+    # formula extracted from "Iman, R.L., Conover, W.J. 1987: A Measure of Top-Down-Correlation. Technometrics Vol.29 (3)"
     
-    k = len(savage_score_array[:, 0])
-    ranking_count = len(savage_score_array[0])
+    n = len(savage_score_array[:, 0])
+    b = len(savage_score_array[0])
         
+    print("Number of parameters:\t\t", n)
+    print("Number of different rankings:\t", b)
+
+    sum_zaehler = 0
+    S1 = 0
+
+    for i in range(0,n):
+        Si = numpy.sum(savage_score_array[i])
+        sum_zaehler += Si**2         
     
-    sum_nenner = 0
-    for i in range(1,k+1):
-        sum_nenner += (1/(i+1))
-        
+        S1 += 1/(i+1)
 
-    sum_savage_scores = numpy.sum(savage_score_array)
-    print("Sum:", sum_savage_scores, sum_nenner)
+    zaehler = sum_zaehler - b**2*(n)
+    nenner = b**2 * (n-S1)
     
-    zaehler = sum_savage_scores - ((ranking_count**2)*k)
-    nenner = ranking_count**2 * (k - sum_nenner)
+    TDCC= zaehler/nenner
 
-    TDCC = zaehler / nenner
-    print("ns_a^2*k", (ranking_count**2)*k)
-    print("zaehler:", zaehler)
-    print("nenner:", nenner)
-    print("TDCC:", TDCC)
+    print("\nTDCC:", TDCC)
+
+    return
 
     
-
-
-        
-
-
 ######################################################
 ######################################################
 ######################################################